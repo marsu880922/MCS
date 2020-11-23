2a1725e9c27c16539a57b45b9cd73064b4866e1d
marsu880922 <74525251+marsu880922@users.noreply.github.com>
2020-11-23 14:32:50 +0800

    Update mcsio.py

--- mcsio.py
+++ mcsio.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+humidity = 1
 while True:
 	SwitchStatus = GPIO.input(24)
 	if( SwitchStatus == 0):
