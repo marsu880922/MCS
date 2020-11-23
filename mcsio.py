334a2be83773c2891c274e6f0d1605140a8df8cf
marsu880922 <74525251+marsu880922@users.noreply.github.com>
2020-11-23 14:59:22 +0800

    Update mcsio.py

--- mcsio.py
+++ mcsio.py
@@ -41,6 +41,8 @@ else:
     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 	
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
